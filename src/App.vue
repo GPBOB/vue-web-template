a12dde67803561f2e1f141f8eda32046484db64a
gp <812117994@qq.com>
2020-11-30 14:45:17 +0800

    页面部分功能添加

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
@@ -9,20 +9,6 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  background-color: #f9f9f9;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./commen.less";
+import element from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(element);
 
 Vue.config.productionTip = false;
 
